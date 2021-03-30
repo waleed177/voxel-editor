840d40af123fb908113f6ed7643120f58a60eddf
waleed alqallaf <waleedalqalaf2@gmail.com>
2021-03-30 22:43:18 +0300

    add to color button

--- ui/dock/Palette.gd
+++ ui/dock/Palette.gd
@@ -7,7 +7,7 @@ onready var color_picker_popup = $ColorPickerPopup
 
 signal color_changed
 
-const MAX_PALETTE_SIZE = 16
+const DEFAULT_PALETTE_SIZE = 16
 const DEFAULT_PALETTE_PATH = "res://addons/waleed_voxels/default_palette.tres"
 var _selected_button: TextureButton = null
 var _selected_button_index: int = -1
@@ -20,9 +20,9 @@ func _ready():
 		_palette_colors = _palette.colors
 	else:
 		_palette_colors = PoolColorArray()
-		for i in MAX_PALETTE_SIZE:
+		for i in DEFAULT_PALETTE_SIZE:
 			_palette_colors.append(Color.white)
-	for i in MAX_PALETTE_SIZE:
+	for i in len(_palette_colors):
 		var texture_button = TextureButton.new()
 		texture_button.texture_normal = preload("res://addons/waleed_voxels/images/16.png")
 		texture_button.modulate = _palette_colors[i]
@@ -35,6 +35,10 @@ func _on_texture_button_gui_input(event, texture_button: TextureButton, i):
 		match event.button_index:
 			BUTTON_LEFT:
 				emit_signal("color_changed", texture_button.modulate)
+			BUTTON_MIDDLE:
+				_palette_colors.remove(i)
+				save_palette()
+				refresh()
 			BUTTON_RIGHT:
 				color_picker_popup.rect_global_position = texture_button.rect_global_position
 				color_picker_popup.popup()
@@ -43,7 +47,25 @@ func _on_texture_button_gui_input(event, texture_button: TextureButton, i):
 func _on_ColorPicker_color_changed(color):
 	_selected_button.modulate = color
 	_palette_colors[_selected_button_index] = color
+	save_palette()
+
+func save_palette():
 	var palette = Palette.new()
 	palette.name = "default"
 	palette.colors = _palette_colors
 	ResourceSaver.save(DEFAULT_PALETTE_PATH, palette)
+
+func _on_AddColor_pressed():
+	_palette_colors.append(Color.white)
+	save_palette()
+	refresh()
+
+func refresh():
+	for child in palette_container.get_children():
+		child.queue_free()
+	for i in len(_palette_colors):
+		var texture_button = TextureButton.new()
+		texture_button.texture_normal = preload("res://addons/waleed_voxels/images/16.png")
+		texture_button.modulate = _palette_colors[i]
+		texture_button.connect("gui_input", self, "_on_texture_button_gui_input", [texture_button, i])
+		palette_container.add_child(texture_button)
